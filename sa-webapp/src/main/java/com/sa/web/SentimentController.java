01a586eecb4f7250b6af6a34bc1cf5503c02e0f2
Shagun Bandi <shabandi@publicisgroupe.net>
2019-12-17 19:31:51 +0530

    Changes

--- sa-webapp/src/main/java/com/sa/web/SentimentController.java
+++ sa-webapp/src/main/java/com/sa/web/SentimentController.java
@@ -41,15 +41,17 @@ public class SentimentController {
         String status = payload.get("status");
         if(status.equalsIgnoreCase("Accept"))
         {
+                 
                 String url = "http://35.223.188.12/api/payment/bank-2";
                 HttpHeaders headers = new HttpHeaders();
                 headers.setContentType(MediaType.APPLICATION_JSON);
-                // headers.set("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5VSTFNRVpHT1RreE5VRXpOalZDTUVSR1FqRkRPVUZDUmtOQk1qUXpOekV4TlVaQlFrWTVPUSJ9.eyJpc3MiOiJodHRwczovL2Rldi05cWxidXBuMy5hdXRoMC5jb20vIiwic3ViIjoiWjlSVmg0Y2NBdTFWUkFFR2pHQlNJZExPeWs1OHQ0cGJAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2VudGltZW50LWFuYWx5c2lzLmlvIiwiaWF0IjoxNTc2NTY0MjM0LCJleHAiOjE1NzY2NTA2MzQsImF6cCI6Ilo5UlZoNGNjQXUxVlJBRUdqR0JTSWRMT3lrNTh0NHBiIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.d5VELxj4PyNIEAy4c-WWayEG0V-i4ZCWCCzBGXgCXRiJlrvVcW816F7J2BbzL9Kx7mu5wzXM9PH0FmGrqTjmXvksr1IuHkXOKw26gPpjeCMjU3ySFZAVS3tz0KCqaGxkz0rH78AjuSEZFBtQHs0Gi3HMy352ZkgvtgPbASBkVwIL4ejRIwUhkeLckbMvlOAvqKGL7KatGkRXfoeRd9uUVDVnwhzQxey_Soj0cCw2gqC2Fw2EAbNC5YUVWJHV7dt4nyqPb814HlJ4ijzfXyLFMhOa-1cn25701l8uCQfb_Eu6oR7IX4moBq-ogPZXf_rGJz6s8LFiuzrHNFQoDjL_aA");
+                headers.set("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5VSTFNRVpHT1RreE5VRXpOalZDTUVSR1FqRkRPVUZDUmtOQk1qUXpOekV4TlVaQlFrWTVPUSJ9.eyJpc3MiOiJodHRwczovL2Rldi05cWxidXBuMy5hdXRoMC5jb20vIiwic3ViIjoiWjlSVmg0Y2NBdTFWUkFFR2pHQlNJZExPeWs1OHQ0cGJAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2VudGltZW50LWFuYWx5c2lzLmlvIiwiaWF0IjoxNTc2NTY0MjM0LCJleHAiOjE1NzY2NTA2MzQsImF6cCI6Ilo5UlZoNGNjQXUxVlJBRUdqR0JTSWRMT3lrNTh0NHBiIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.d5VELxj4PyNIEAy4c-WWayEG0V-i4ZCWCCzBGXgCXRiJlrvVcW816F7J2BbzL9Kx7mu5wzXM9PH0FmGrqTjmXvksr1IuHkXOKw26gPpjeCMjU3ySFZAVS3tz0KCqaGxkz0rH78AjuSEZFBtQHs0Gi3HMy352ZkgvtgPbASBkVwIL4ejRIwUhkeLckbMvlOAvqKGL7KatGkRXfoeRd9uUVDVnwhzQxey_Soj0cCw2gqC2Fw2EAbNC5YUVWJHV7dt4nyqPb814HlJ4ijzfXyLFMhOa-1cn25701l8uCQfb_Eu6oR7IX4moBq-ogPZXf_rGJz6s8LFiuzrHNFQoDjL_aA");
                 Map<String,String> map=new HashMap<String, String>();
                 map.put("network","visa");
                 HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
                 RestTemplate restTemplate=new RestTemplate();
                 ResponseEntity<String> response = restTemplate.postForEntity(url, request , String.class);
+               
                 return "Response from issuing bank "+response.getBody();
         }
         return "network declined";
